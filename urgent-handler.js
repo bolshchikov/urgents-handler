b68a229d05e4001937764afa9b07285b041f204d
Sergey Bolshchikov <sergey@bolshchikov.net>
2020-11-22 11:21:16 +0200

    Update urgent-handler.js

--- urgent-handler.js
+++ urgent-handler.js
@@ -1,22 +1,25 @@
-import { analyzeImpact, readLogs } from './monitoring';
+import { analyzeImpact } from './monitoring';
 import { canPerformBasicAction } from './bi';
 import { notifyUrgentChannel } from './notifications';
 import { canRollbackHelp, rollback } from './releases';
-import { fix } from './codebase';
+import { hotfix, fix } from './codebase';
+import { addTicket } from './backlog';
+import { MITIGATION_STRATEGIES, applyMitigationStrategy } from './mitigations';
 
 function defineProblem(compain, monitoring) {
   const issue = {
     // some black magic here
   };
-  const mitigated = () => {
+  const isMitigated = () => {
     // shows whether the current problem is resolved
   };
-  const solved = () => {
+  const isSolved = () => {
     // shows whether the underlying reason is solved
   };
   return {
     issue,
-    resolved
+    isMitigated,
+    isSolved
   }
 }
 
@@ -32,32 +35,43 @@ function isReallyUrgent(issue) {
 }
 
 function mitigateUrgent(issue) {
+  const mitigated = false;
   if (canRollbackHelp(issue)) {
-    return rollback();
+    mitigated = applyMitigationStrategy(MITIGATION_STRATEGIES.ROLLBACK); // 1. the quickest
   }
+  if (!mitigated) {
+    mitigated = applyMitigationStrategy(MITIGATION_STRATEGIES.DATA_FIX); // 2. second quickest but little but risky
+  }
+  if (!mitigated) {
+    mitigated = applyMitigationStrategy(MITIGATION_STRATEGIES.HOT_FIX); // 3. takes time
+  }
+  return mitigated;
 }
 
 function main(compain, monitoring) {
   console.warn('Take a deep breath');
   
-  const {issue, resolved} = defineProblem(compain, monitoring);
+  const {issue, isMitigated, isSolved} = defineProblem(compain, monitoring);
   
   if (isReallyUrgent(potentialIssue)) {
     notifyUrgentChannel();
-    while(!mitigated(issue)) {
-      mitigateUrgent(issue);
-    }
-  }
 
-  const severity = estimateSeverity(issue);
-  
-  switch(severity) {
-    case 'high':
-      break;
-    case 'medium':
-      break;
-    case 'low':
-      break;
+    let mitigationAttempt = 0;
+    let MAX_MITIGATION_ATTEMPTS = MITIGATION_STRATEGIES.length;
+    
+    while(!isMitigated(issue) || mitigationAttempt <= MAX_MITIGATION_ATTEMPTS) {
+      mitigateUrgent(issue);
+      mitigationAttempt += 1;
+    }
+    
+    if (!isSolved(issue)) {
+      try {
+        fix() // 4. if all previous options failed
+      }
+      catch (ooops) {
+        addTicket(issue);
+      }
+    }
   }
 }
 
