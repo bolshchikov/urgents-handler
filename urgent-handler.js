2304b56c64f36a19b0ff54d3a69e0ad2b31a4cc1
Sergey Bolshchikov <sergey@bolshchikov.net>
2020-11-22 08:30:48 +0200

    Update urgent-handler.js

--- urgent-handler.js
+++ urgent-handler.js
@@ -1,3 +1,24 @@
-function urgentHandler(user, monitoring) {}
+import { analyzeImpact } from 'monitoring';
+import { canPerformBasicAction } from 'bi';
+
+function defineProblem(compain, monitoring) {
+  // some black magic going on here
+}
+
+function isReallyUrgent(issue) {
+  const impact = analyzeImpact(issue);
+  if (impact >= '5% of all users' || impact >= '50% of the same company') {
+    return true;
+  }
+  if (!canPerformBasicAction(issue)) {
+    return true;
+  }
+  return false;
+}
+
+function urgentHandler(compain, monitoring) {
+  const potentialIssue = defineProblem(compain, monitoring);
+  if (isReallyUrgent(potentialIssue)) {}
+}
 
 export default urgentHandler;
